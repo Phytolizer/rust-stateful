15b4988f18f459a178573573893883ea953fe11b
Kyle Coffey <mariorules24@gmail.com>
2020-08-05 13:49:29 -0500

    Clean up API

--- src/lib.rs
+++ src/lib.rs
@@ -1,82 +1,40 @@
+use crossbeam::{bounded, SendError, TryRecvError, RecvError};
 use crossbeam_channel::Receiver;
 use crossbeam_channel::Select;
 use crossbeam_channel::Sender;
 use crossterm::terminal;
 use crossterm::terminal::ClearType;
 use crossterm::{cursor, execute, style};
-use io::{stdout, BufRead, BufReader, Read, Write};
+use io::Write;
 use lazy_static::lazy_static;
 use parking_lot::Condvar;
 use parking_lot::Mutex;
 use std::{
     io,
-    process::{Command, Output, Stdio},
-    sync::Arc,
     thread::{self, JoinHandle},
     time::Duration,
 };
 
-fn child_stream_to_lines<R>(stream: R) -> Arc<Mutex<Vec<u8>>>
-where
-    R: Read + Send + 'static,
-{
-    let out = Arc::new(Mutex::new(vec![]));
-    let vec = out.clone();
-    let mut reader = BufReader::new(stream);
-    thread::Builder::new()
-        .name("child_stream_to_vec".into())
-        .spawn(move || loop {
-            let mut buf = Vec::with_capacity(1);
-            match reader.read_until(b'\n', &mut buf) {
-                Err(e) => {
-                    eprintln!("{}] Error reading from stream: {}", line!(), e);
-                    break;
-                }
-                Ok(got) => match got {
-                    0 => break,
-                    _ => *vec.lock() = buf,
-                },
-            }
-        })
-        .expect("!thread");
-    out
-}
-
-pub fn run_with_status(
-    mut command: Command,
-    deactivate_sender: &Sender<()>,
-    show_output: bool,
-) -> io::Result<Output> {
-    if show_output {
-        command.stdout(Stdio::piped()).stderr(Stdio::piped());
+pub fn run_closure<T, F: Fn() -> T>(
+    message: Option<&str>,
+    done_message: Option<&str>,
+    communicator: SpinnerCommunicator,
+    closure: F,
+) -> Result<T, SendError<()>> {
+    if let Some(message) = message {
+        communicator
+            .send_status_message(message)
+            .map_err(|_| SendError(()))?;
     }
-    let mut child = command.spawn()?;
-    if show_output {
-        let out = child_stream_to_lines(child.stdout.take().unwrap());
-        let err = child_stream_to_lines(child.stderr.take().unwrap());
-        while child.try_wait()? == None {
-            let mut out_lock = out.lock();
-            let mut err_lock = err.lock();
-            if !out_lock.is_empty() || !err_lock.is_empty() {
-                let mut stdout = stdout();
-                execute!(
-                    stdout,
-                    terminal::Clear(ClearType::CurrentLine),
-                    cursor::MoveToColumn(0),
-                )
-                .unwrap();
-                stdout.write_all(&out_lock)?;
-                stdout.write_all(&err_lock)?;
-                stdout.flush()?;
-                // stdout.write_all(b"\n")?;
-            }
-            out_lock.clear();
-            err_lock.clear();
-        }
+    if let Some(done_message) = done_message {
+        communicator
+            .send_done_message(done_message)
+            .map_err(|_| SendError(()))?;
     }
-    let output = child.wait_with_output()?;
-    deactivate_sender.send(()).expect("!send");
-    Ok(output)
+    communicator.activate()?;
+    let res = closure();
+    communicator.deactivate()?;
+    Ok(res)
 }
 
 pub struct SpinnerThreads {
@@ -89,43 +47,159 @@ lazy_static! {
 }
 static SPINNER_TICKER: Condvar = Condvar::new();
 
-fn spinner_thread(
-    spinner_chars: Vec<char>,
-    message_receiver: Option<Receiver<String>>,
-    done_message_receiver: Option<Receiver<String>>,
+pub struct Spinner {
+    states: Vec<String>,
+    tick_interval: Duration,
+    message_receiver: Receiver<String>,
+    done_message_receiver: Receiver<String>,
     activator: Receiver<()>,
     deactivator: Receiver<()>,
     stopper: Receiver<()>,
-) {
-    let mut i: usize = 0;
-    let mut selector = Select::new();
-    let op1 = selector.recv(&activator);
-    let op2 = selector.recv(&stopper);
-    'spinner: loop {
-        let op = selector.select();
-        match op.index() {
-            i if i == op1 => op.recv(&activator).unwrap(),
-            i if i == op2 => {
-                op.recv(&stopper).unwrap();
-                break 'spinner;
-            }
-            _ => unreachable!(),
+    state_index: usize,
+}
+
+impl Spinner {
+    fn new(
+        states: &[&str],
+        tick_interval: Duration,
+        message_receiver: Receiver<String>,
+        done_message_receiver: Receiver<String>,
+        activator: Receiver<()>,
+        deactivator: Receiver<()>,
+        stopper: Receiver<()>,
+    ) -> Self {
+        assert!(states.len() > 1);
+        Self {
+            states: states.iter().map(|s| s.to_string()).collect(),
+            tick_interval,
+            message_receiver,
+            done_message_receiver,
+            activator,
+            deactivator,
+            stopper,
+            state_index: 0,
         }
-        let message = if let Some(ref message_receiver) = message_receiver {
-            message_receiver.recv().unwrap()
-        } else {
-            String::new()
-        };
-        let done_message = if let Some(ref done_message_receiver) = done_message_receiver {
-            done_message_receiver.recv().unwrap()
-        } else {
-            String::new()
-        };
-        while deactivator.try_recv().is_err() {
-            {
-                let mut lock = SPINNER_MUTEX.lock();
-                SPINNER_TICKER.wait(&mut lock);
+    }
+
+    fn wait_for_tick(&mut self) {
+        let mut lock = SPINNER_MUTEX.lock();
+        SPINNER_TICKER.wait(&mut lock);
+        self.state_index = (self.state_index + 1) % (self.states.len() - 1);
+    }
+
+    fn state(&self) -> &str {
+        &self.states[self.state_index]
+    }
+
+    fn done_state(&self) -> &str {
+        &self.states.last().unwrap()
+    }
+}
+
+pub struct SpinnerCommunicator {
+    activate_sender: Sender<()>,
+    deactivate_sender: Sender<()>,
+    status_message_sender: Sender<String>,
+    done_message_sender: Sender<String>,
+    stop_sender: Sender<()>,
+}
+
+impl SpinnerCommunicator {
+    fn new(
+        activate_sender: Sender<()>,
+        deactivate_sender: Sender<()>,
+        status_message_sender: Sender<String>,
+        done_message_sender: Sender<String>,
+        stop_sender: Sender<()>,
+    ) -> Self {
+        Self {
+            activate_sender,
+            deactivate_sender,
+            status_message_sender,
+            done_message_sender,
+            stop_sender,
+        }
+    }
+
+    pub fn activate(&self) -> Result<(), SendError<()>> {
+        self.activate_sender.send(())
+    }
+    pub fn deactivate(&self) -> Result<(), SendError<()>> {
+        self.deactivate_sender.send(())
+    }
+    pub fn send_status_message(&self, msg: &str) -> Result<(), SendError<String>> {
+        self.status_message_sender.send(msg.to_string())
+    }
+    pub fn send_done_message(&self, msg: &str) -> Result<(), SendError<String>> {
+        self.done_message_sender.send(msg.to_string())
+    }
+    pub fn stop(&self) -> Result<(), SendError<()>> {
+        self.stop_sender.send(())
+    }
+}
+
+pub fn spawn_spinner(states: &[&str], tick_interval: Duration) -> SpinnerCommunicator {
+    let (message_sender, message_receiver) = bounded::<String>(1);
+    let (done_message_sender, done_message_receiver) = bounded::<String>(1);
+    let (activate_sender, activator) = bounded::<()>(1);
+    let (deactivate_sender, deactivator) = bounded::<()>(1);
+    let (stop_sender, stopper) = bounded::<()>(1);
+    let spinner = Spinner::new(
+        states,
+        tick_interval,
+        message_receiver,
+        done_message_receiver,
+        activator,
+        deactivator,
+        stopper.clone(),
+    );
+
+    thread::spawn(move || ticker_thread(tick_interval, stopper));
+    thread::spawn(move || spinner_thread(spinner));
+
+    SpinnerCommunicator::new(
+        activate_sender,
+        deactivate_sender,
+        message_sender,
+        done_message_sender,
+        stop_sender,
+    )
+}
+
+fn receive_multiple<T>(rs: &[&Receiver<T>]) -> Result<usize, RecvError> {
+    let mut sel = Select::new();
+    for r in rs {
+        sel.recv(r);
+    }
+
+    let op = sel.select();
+    let i = op.index();
+    op.recv(&rs[i])?;
+    Ok(i)
+}
+
+fn spinner_thread(mut spinner: Spinner) {
+    'spinner: loop {
+        match receive_multiple(&[&spinner.activator, &spinner.stopper]) {
+            Ok(i) => match i {
+                0 => {},
+                1 => break 'spinner,
+                _ => unreachable!(),
             }
+            Err(_) => continue,
+        }
+        let message = match spinner.message_receiver.try_recv() {
+            Ok(m) => m,
+            Err(TryRecvError::Empty) => String::new(),
+            Err(e) => panic!(e),
+        };
+        let done_message = match spinner.done_message_receiver.try_recv() {
+            Ok(m) => m,
+            Err(TryRecvError::Empty) => String::new(),
+            Err(e) => panic!(e),
+        };
+        while spinner.deactivator.try_recv().is_err() {
+            spinner.wait_for_tick();
             {
                 let mut stderr = io::stderr();
                 execute!(
@@ -135,11 +209,10 @@ fn spinner_thread(
                     style::SetForegroundColor(style::Color::Green),
                 )
                 .unwrap();
-                write!(stderr, "{}", spinner_chars[i]).unwrap();
+                write!(stderr, "{}", spinner.state()).unwrap();
                 execute!(stderr, style::ResetColor {},).unwrap();
                 write!(stderr, " {}", message).unwrap();
             }
-            i = (i + 1) % (spinner_chars.len() - 1);
         }
         let mut stderr = io::stderr();
         execute!(
@@ -149,9 +222,9 @@ fn spinner_thread(
             style::SetForegroundColor(style::Color::Green),
         )
         .unwrap();
-        write!(stderr, "{}", spinner_chars[spinner_chars.len() - 1]).unwrap();
+        write!(stderr, "{}", spinner.done_state()).unwrap();
         execute!(stderr, style::ResetColor {},).unwrap();
-        write!(stderr, " {}", done_message).unwrap();
+        writeln!(stderr, " {}", done_message).unwrap();
     }
 }
 
@@ -163,29 +236,12 @@ fn ticker_thread(interval: Duration, stopper: Receiver<()>) {
 }
 
 impl SpinnerThreads {
-    pub fn new<S: AsRef<str> + Send + Sync>(
-        spinner_chars: S,
-        spinner_tick_interval: Duration,
-        status_message_receiver: Option<Receiver<String>>,
-        done_message_receiver: Option<Receiver<String>>,
-        spinner_activator: Receiver<()>,
-        spinner_deactivator: Receiver<()>,
-        stopper: Receiver<()>,
-    ) -> Self {
-        let stopper_2 = stopper.clone();
-        let spinner_chars: Vec<char> = spinner_chars.as_ref().chars().collect();
+    pub fn new(spinner: Spinner) -> Self {
+        let stopper_2 = spinner.stopper.clone();
+        let tick_interval = spinner.tick_interval;
         Self {
-            _spinner_thread: thread::spawn(move || {
-                spinner_thread(
-                    spinner_chars,
-                    status_message_receiver,
-                    done_message_receiver,
-                    spinner_activator,
-                    spinner_deactivator,
-                    stopper,
-                )
-            }),
-            _ticker_thread: thread::spawn(move || ticker_thread(spinner_tick_interval, stopper_2)),
+            _spinner_thread: thread::spawn(move || spinner_thread(spinner)),
+            _ticker_thread: thread::spawn(move || ticker_thread(tick_interval, stopper_2)),
         }
     }
 }
@@ -193,7 +249,7 @@ impl SpinnerThreads {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crossbeam_channel::unbounded;
+    use std::process::Command;
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
@@ -201,30 +257,24 @@ mod tests {
 
     #[test]
     fn test_run() {
-        let (status_message_sender, status_message_receiver) = unbounded::<String>();
-        let (done_message_sender, done_message_receiver) = unbounded::<String>();
-        let (spinner_activator_sender, spinner_activator) = unbounded::<()>();
-        let (spinner_deactivator_sender, spinner_deactivator) = unbounded::<()>();
-        let (stop_sender, stopper) = unbounded::<()>();
-        let _threads = SpinnerThreads::new(
-            "/-\\|*",
-            Duration::from_millis(100),
-            Some(status_message_receiver),
-            Some(done_message_receiver),
-            spinner_activator,
-            spinner_deactivator,
-            stopper,
-        );
+        let spinner_communicator =
+            spawn_spinner(&["/", "-", "\\", "|", "*"], Duration::from_millis(100));
 
-        let mut command = Command::new("yay");
-        command.arg("-Si").arg("test");
-        status_message_sender.send("Sleeping...".into()).unwrap();
-        done_message_sender.send("Slept!".into()).unwrap();
-        spinner_activator_sender.send(()).unwrap();
-        run_with_status(command, &spinner_deactivator_sender, true).unwrap();
+        let res = run_closure(
+            Some("Sleeping..."),
+            Some("Slept"),
+            spinner_communicator,
+            || {
+                Command::new("sleep")
+                    .arg("1")
+                    .spawn()
+                    .unwrap()
+                    .wait()
+                    .unwrap()
+            },
+        )
+        .unwrap();
 
-        thread::sleep(Duration::from_millis(100));
-
-        stop_sender.send(()).unwrap();
+        assert!(res.success());
     }
 }
